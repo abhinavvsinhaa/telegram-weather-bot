8715cbbee3c19fcb75e2d733e651315589f9a1af
Abhinav Sinha <abhinavvsinhaa@gmail.com>
2024-01-08 18:18:56 +0530

    Added config module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TelegramModule } from './telegram/telegram.module';
 
 @Module({
-  imports: [TelegramModule],
+  imports: [ConfigModule.forRoot(), TelegramModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
