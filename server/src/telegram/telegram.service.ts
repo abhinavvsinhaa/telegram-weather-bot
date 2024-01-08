d6a219969c1950c57a439e118e5892d43ef64754
Abhinav Sinha <abhinavvsinhaa@gmail.com>
2024-01-08 19:17:46 +0530

    Passed user model in constructor

--- server/src/telegram/telegram.service.ts
+++ server/src/telegram/telegram.service.ts
@@ -1,4 +1,8 @@
 import { Injectable, Logger } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './schema/user.schema';
+import { Model } from 'mongoose';
+
 const TelegramBot = require('node-telegram-bot-api');
 
 @Injectable()
@@ -7,12 +11,17 @@ export class TelegramService {
     private logger = new Logger(TelegramService.name)
     private readonly TELEGRAM_TOKEN: string = process.env.TELEGRAM_BOT_KEY
 
-    constructor() {
+    constructor(@InjectModel(User.name) private userModel: Model<User>) {
         this.bot = new TelegramBot(this.TELEGRAM_TOKEN, { polling: true })
 
+        this.bot.onText(/\/subscribe (.+)/, this.onSubscribe)
         this.bot.on("message", this.onReceiveMessage)
     }
     
+    onSubscribe = (message: any, match: string) => {
+
+    }
+
     onReceiveMessage = (message: any) => {
         this.logger.debug(message)
     }
