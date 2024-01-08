16aa5aa36ada4972c38f57e80b63eecbd4771da5
Abhinav Sinha <abhinavvsinhaa@gmail.com>
2024-01-08 18:20:54 +0530

    Instantiated Telegram Bot and created receive message and send message handlers

--- server/src/telegram/telegram.service.ts
+++ server/src/telegram/telegram.service.ts
@@ -1,4 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+const TelegramBot = require('node-telegram-bot-api');
 
 @Injectable()
-export class TelegramService {}
+export class TelegramService {
+    private readonly bot: any
+    private logger = new Logger(TelegramService.name)
+    private readonly TELEGRAM_TOKEN: string = process.env.TELEGRAM_BOT_KEY
+
+    constructor() {
+        this.bot = new TelegramBot(this.TELEGRAM_TOKEN, { polling: true })
+
+        this.bot.on("message", this.onReceiveMessage)
+    }
+    
+    onReceiveMessage = (message: any) => {
+        this.logger.debug(message)
+    }
+
+    sendMessage = (chatId: string, message: string) => {
+        this.bot.sendMessage(chatId, message)
+    }
+}
