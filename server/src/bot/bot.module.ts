d2a6401f2f048c00207b019b4bdb916213348755
Abhinav Sinha <abhinavvsinhaa@gmail.com>
2024-01-09 18:59:59 +0530

    bug fixes

--- server/src/bot/bot.module.ts
+++ server/src/bot/bot.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { BotController } from './bot.controller';
 import { BotService } from './bot.service';
+import { PrismaService } from 'prisma.service';
 
 @Module({
   controllers: [BotController],
-  providers: [BotService]
+  providers: [PrismaService, BotService]
 })
 export class BotModule {}
