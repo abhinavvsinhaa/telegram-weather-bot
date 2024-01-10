0dd1b829a31ba4cff2dfa88b577e8ee0e61117cc
Abhinav Sinha <abhinavvsinhaa@gmail.com>
2024-01-10 14:33:00 +0530

    enable cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,9 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: ['http://localhost:3000', 'https://telegram-weather-bot-rust.vercel.app/']
-  })
+  app.enableCors()
   await app.listen(8080);
 }
 bootstrap();
